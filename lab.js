3e2f2b751e493140eb61ef520339114dc7f74e6f
Nicholas Phillips <phillipssnicholas@gmail.com>
2022-01-19 17:55:52 -0700

    Initial commit

--- lab.js
+++ lab.js
@@ -7,7 +7,12 @@
 */
 
 //CODE HERE
-
+let lovesCode = true
+if (lovesCode === true){
+  console.log ("I love to code!")
+}else{
+  console.log ("Coding has its challenges.")
+}
 // For problems 2-3 use the following lines of code:
 var amysAge = 29
 var brittanisAge = 34
@@ -21,7 +26,13 @@ var brittanisBirthYear = 1986
 */
 
 //CODE HERE
-
+if (amysAge > brittanisAge){
+console.log ("Amy's age is greater then Brittani's age.")
+}else if(amysAge === brittanisAge){
+  console.log ("Amy and Brittani are the same age.")
+}else{
+  console.log ("Brittani's age is greater than Amy's age.")
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -30,12 +41,17 @@ var brittanisBirthYear = 1986
 */
 
 //CODE HERE
+if (amysBirthYear === brittanisBirthYear){
+console.log (`Amy and Brittani were born in the same year!`)
+}else{
+  console.log ('Brittani and Amy were born in different years. Lame.')
 
+}
 
 ////////// PROBLEM 4 //////////
 
-let temperature = 55
-let rain = true
+let temperature = 85
+let rain = false
 
 /*
   Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
@@ -43,7 +59,27 @@ let rain = true
 */
 
 //CODE HERE
+if (temperature > 80 && rain === true){
+console.log(`Wear a t-shirt and bring an umbrella.`)
 
+}else if(temperature >=60 && temperature <=80 && rain === true){
+console.log("Wear a rain jacket, and bring an umbrella.")
+
+}else if(temperature <60 && rain === true){
+console.log(`Wear a jacket and bring an umbrella.`)
+
+}else if(temperature <=32 && rain === true){
+ console.log("Wear a snow coat.")
+
+}else if(temperature >=60 && temperature <=80 && rain !== true){
+  console.log("Wear a  jacket")
+  
+  }else if(temperature <60 && rain !== true){
+  console.log(`Wear a coat.`)
+  
+  }else if(temperature > 80 && rain !== true){
+   console.log("Wear a t-shirt.")
+  }
 
 ////////// PROBLEM 5 //////////
 
@@ -52,8 +88,9 @@ let rain = true
 */
 
 //CODE HERE
-
-
+for(let i = 0; i<10; i++){
+console.log('hello.')
+};
 ////////// PROBLEM 6 //////////
 
 /*
@@ -61,7 +98,8 @@ let rain = true
 */
 
 //CODE HERE
-
+for(let i = 1; i<11; i += 1)
+console.log(i)
 
 ////////// PROBLEM 7 //////////
 
@@ -70,12 +108,20 @@ let rain = true
 */
 
 //CODE HERE
+for(let i = 10; i>=0; i--)
+console.log(i)
+
+
+
+////////// PROBLEM 8 //////////
 
 let score = 0
 let passingScore = 7
 
-////////// PROBLEM 8 //////////
-
+while(score < passingScore){
+console.log("Your score is not high enough")
+score += 1
+}
 /*
   Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
 */
@@ -92,13 +138,24 @@ let passingScore = 7
 */
 
 //CODE HERE
+let changeMyMind = true
+  
+  if(changeMyMind === true){
+    changeMyMind = false
+    console.log(changeMyMind);
+    
+
+  }else{
+    changeMyMind = true
+    console.log(changeMyMind);
+  }
 
 ////////// PROBLEM 10 //////////
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
 //CODE HERE
-
-
+changeMyMind = !changeMyMind
+console.log(changeMyMind);
 
 ////////// ADVANCED PROBLEMS //////////
 
@@ -108,3 +165,38 @@ let z = 5
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
+while(z > 0){
+  for(z; z > 0; z -= 1)
+  
+  console.log(z)
+  
+}
+let q = 4
+
+while(q > 0){
+  for(q; q > 0; q -=1)
+
+  console.log(q)
+}
+
+let t = 3
+
+while(t > 0){
+  for(t; t > 0; t -=1)
+
+  console.log(t)
+}
+let r = 2
+
+while(r > 0){
+  for(r; r > 0; r -=1)
+
+  console.log(r)
+}
+let k = 1
+
+while(k > 0){
+  for(k; k > 0; k -=1)
+
+  console.log(k)
+}
